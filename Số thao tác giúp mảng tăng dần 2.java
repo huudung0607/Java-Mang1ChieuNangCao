1e2971ad136266c12ac69ca1ac9b508e89cdd703
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-26 08:09:54 +0700

    Create Số thao tác giúp mảng tăng dần 2.java

---
+++ Số thao tác giúp mảng tăng dần 2.java
@@ -0,0 +1,29 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt(), d = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0 ; i < n ; i++) {
+            a[i] = sc.nextInt();
+        }
+        long sum = 0;
+        for (int i = 1 ; i < n ; i++) {
+            if (a[i - 1] >= a[i]) {
+                int res = a[i - 1] - a[i] + 1;
+                int tt = (res + d - 1) / d;
+                a[i] += tt * d;
+                sum += tt;
+            }
+        }
+        System.out.print(sum);
+    }
+}
