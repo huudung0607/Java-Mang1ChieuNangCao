adbdb4f4ecc5775b524c9ea4036b5317cd497da2
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-28 11:22:15 +0700

    Create Maximum pair.java

---
+++ Maximum pair.java
@@ -0,0 +1,21 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.print(Math.max(Math.abs(a[0] + a[1]),Math.abs(a[n - 1] + a[n - 2])));
+    }
+}
